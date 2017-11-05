43df1dae44fa04da47b09c5de7867f087bb5f146
chibitofu <erinmoony@gmail.com>
2017-11-05 03:36:35 -0800

    Initial Commit

---
+++ game_safe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  game_safe
+//
+//  Created by Erin Moon on 11/5/17.
+//  Copyright © 2017 Erin Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
